3e984af7acd1dfe1cf0a834562b291c9c245a2ef
Arvindkr123 <thakurarvindkr10@gmail.com>
2025-12-13 20:19:05 +0530

    feat: add Discord message component and mock UI
    - Implemented `DiscordMessage` component for displaying messages with avatar, username, timestamp, and content.
    - Created `Icons` component for rendering SVG icons, including a Discord icon.
    - Developed `MockDiscordUI` component to simulate the Discord interface with server and DM lists.
    - Introduced `AnimatedList` and `AnimatedListItem` components for animated rendering of lists.
    - Added styles and structure to enhance the visual representation of the Discord-like UI.

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -3,6 +3,9 @@ import MaxWidthWrapper from "@/app/components/max-width_wrapper"
 import Heading from "@/app/components/heading"
 import { Check } from "lucide-react"
 import { ShinyButton } from "@/app/components/shiny-button"
+import { MockDiscordUI } from "../components/mock-discord-ui"
+import { AnimatedList, AnimatedListItem } from "@/components/ui/animated-list"
+import { DiscordMessage } from "../components/discord-message"
 
 const Home = () => {
   return (
@@ -47,7 +50,58 @@ const Home = () => {
           </div>
         </MaxWidthWrapper>
       </section>
-      <section></section>
+      <section className="relative bg-brand-25 pb-4">
+        <div className="absolute inset-x-0 bottom-24 top-24 bg-brand-700" />
+        <div className="relative mx-auto">
+          <MaxWidthWrapper className="relative">
+            <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
+              <MockDiscordUI>
+                <AnimatedList>
+                  <DiscordMessage 
+                    avatarSrc="/brand-asset-profile-picture.png"
+                    avatarAlt="PingPanda Avatar"
+                    username="PingPanda"
+                    timestamp="Today at 12:35PM"
+                    badgeText="SignUp"
+                    badgeColor="#43b581"
+                    title="👤 New user signed up"
+                    content={{
+                      name:'Arvind Thakur', 
+                      email:'arvind@gmail.com',
+                    }}
+                   />
+                  <DiscordMessage 
+                    avatarSrc="/brand-asset-profile-picture.png"
+                    avatarAlt="PingPanda Avatar"
+                    username="PingPanda"
+                    timestamp="Today at 12:35PM"
+                    badgeText="SignUp"
+                    badgeColor="#43b581"
+                    title="👤 New user signed up"
+                    content={{
+                      name:'Arvind Thakur', 
+                      email:'arvind@gmail.com',
+                    }}
+                   />
+                  <DiscordMessage 
+                    avatarSrc="/brand-asset-profile-picture.png"
+                    avatarAlt="PingPanda Avatar"
+                    username="PingPanda"
+                    timestamp="Today at 12:35PM"
+                    badgeText="SignUp"
+                    badgeColor="#43b581"
+                    title="👤 New user signed up"
+                    content={{
+                      name:'Arvind Thakur', 
+                      email:'arvind@gmail.com',
+                    }}
+                   />
+                </AnimatedList>
+              </MockDiscordUI>
+            </div>
+          </MaxWidthWrapper>
+        </div>
+      </section>
       <section></section>
       <section></section>
     </>
