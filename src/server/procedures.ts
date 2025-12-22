f9a84aabc239508e1aab5fb370cc981f5a7db0dd
Arvindkr123 <thakurarvindkr10@gmail.com>
2025-12-23 00:02:49 +0530

    feat: implement dashboard page with category management and modal for deletion

--- src/server/procedures.ts
+++ src/server/procedures.ts
@@ -1,41 +1,44 @@
+import { db } from "@/db";
 import { j } from "./__internals/j"
+import { currentUser } from "@clerk/nextjs/server";
+import { HTTPException } from "hono/http-exception";
 
-/**
- * Middleware for providing a built-in cache with your Prisma database.
- *
- * You can remove this if you don't like it, but caching can massively speed up your database queries.
- */
+const authMiddleware = j.middleware(async ({ c, next }) => {
+    const authHeader = c.req.header('Authorization');
+    if (authHeader) {
+        const apiKey = authHeader.split(" ")[1];
 
-// const extendedDatabaseMiddleware = j.middleware(async ({ c, next }) => {
-//   const variables = env(c)
+        const user = await db.user.findUnique(
+            {
+                where: {
+                    apiKey
+                }
+            }
+        )
 
-//   const pool = new Pool({
-//     connectionString: variables.DATABASE_URL,
-//   })
+        if (user) {
+            return next({user});
+        }
+    }
 
-//   const adapter = new PrismaNeon(pool)
+    const auth = await currentUser();
+    if (!auth) {
+        throw new HTTPException(401, {
+            message: 'Unauthorized'
+        })
+    }
 
-//   const redis = new Redis({
-//     token: variables.REDIS_TOKEN,
-//     url: variables.REDIS_URL,
-//   })
-
-//   const db = new PrismaClient({
-//     adapter,
-//   }).$extends(cacheExtension({ redis }))
-
-//   // Whatever you put inside of `next` is accessible to all following middlewares
-//   return await next({ db })
-// })
-
-/**
- * Public (unauthenticated) procedures
- *
- * This is the base piece you use to build new queries and mutations on your API.
- */
-
-const authMiddleware = j.middleware(({next})=>{
-    return next({});
+    const user = await db.user.findUnique({
+        where: {
+            externalId: auth.id
+        }
+    });
+    if (!user) {
+        throw new HTTPException(401, {
+            message: 'Unauthorized'
+        })
+    }
+    return next({user});
 })
 
 export const baseProcedure = j.procedure
